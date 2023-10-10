c006b528ee1876161f8b7b302aae86bc9c98ad12
iria <iria.kal@gmail.com>
2023-10-10 22:19:09 +0300

    sql script

---
+++ film_actor.sql
@@ -0,0 +1,17 @@
+SELECT f.title as 'movie', count(fa.actor_id) as 'total_actors'
+from film f
+join film_actor fa
+on f.film_id=fa.film_id
+group by f.title;
+
+select a.first_name ,a.last_name, count(f.film_id)
+as 'total_films'
+from actor a
+join film_actor f on
+a.actor_id=f.actor_id
+group by a.actor_id
+having total_films>10;
+
+
+select * from film 
+where title like 'K_%_%_%'
